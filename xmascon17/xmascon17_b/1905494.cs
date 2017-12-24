656e52f740a930a838e28189818ddd7744cdd886
key-moon <kymn0116@gmail.com>
2017-12-24 10:58:10 +0000

    add xmascon17/xmascon17_b/1905494.cs

---
+++ xmascon17/xmascon17_b/1905494.cs
@@ -0,0 +1,48 @@
+// detail: https://atcoder.jp/contests/xmascon17/submissions/1905494
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class P
+{
+    static void Main()
+    {
+        string res = "17 16
+";
+        res += "*...............
+";
+        res += "X7..............
+";
+        res += ".m1.............
+";
+        res += "..a0............
+";
+        res += "...s2...........
+";
+        res += "....Ct..........
+";
+        res += ".....os.........
+";
+        res += "......ne........
+";
+        res += ".......tt.......
+";
+        res += "........en......
+";
+        res += ".........so.....
+";
+        res += "..........tC....
+";
+        res += "...........2s...
+";
+        res += "............0a..
+";
+        res += ".............1m.
+";
+        res += "..............7X
+";
+        res += "...............*";
+        Console.WriteLine(res);
+    }
+}
