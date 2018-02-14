c8115efc75ce20b18ef10f8732059842dee49751
key-moon <kymn0116@gmail.com>
2018-02-14 14:37:40 +0000

    add tenka1-2014-quala/tenka1_2014_qualA_a/2095259.cs

---
+++ tenka1-2014-quala/tenka1_2014_qualA_a/2095259.cs
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/tenka1-2014-quala/submissions/2095259
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class P
+{
+    static void Main()
+    {
+        Console.WriteLine(string.Join("
+", Enumerable.Range(1, 1000).Select(x => x.ToString()).OrderBy(x => x).ToArray()));
+    }
+}
