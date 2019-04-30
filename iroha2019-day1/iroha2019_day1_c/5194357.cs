be1114d6851758568d4074454d41ca11e79096df
key-moon <kymn0116@gmail.com>
2019-04-30 04:02:45 +0000

    add iroha2019-day1/iroha2019_day1_c/5194357.cs

---
+++ iroha2019-day1/iroha2019_day1_c/5194357.cs
@@ -0,0 +1,20 @@
+// detail: https://atcoder.jp/contests/iroha2019-day1/submissions/5194357
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+static class P
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(string.Join("
+", Enumerable.Range(n - 7, 8)));
+    }
+}
