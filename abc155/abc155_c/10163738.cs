1477afeb796433cb17e9ca510e6cbfafe2093b11
key-moon <kymn0116@gmail.com>
2020-02-16 14:00:48 +0000

    add abc155/abc155_c/10163738.cs

---
+++ abc155/abc155_c/10163738.cs
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc155/submissions/10163738
+using System;
+using System.Linq;
+
+public static class P
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(string.Join("
+", Enumerable.Repeat(0, n).Select(_ => Console.ReadLine()).GroupBy(x => x).GroupBy(x => x.Count()).OrderByDescending(x => x.Key).First().Select(x => x.Key).OrderBy(x => x, StringComparer.OrdinalIgnoreCase)));
+    }
+}
